7bc6138ffb6acedd9fd9ac1944d04f0de1685789
Max Babich <sapienti@list.ru>
2022-12-14 15:25:38 +0300

    example004 added

--- example004_Div/Program.cs
+++ example004_Div/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the fist namber");
+string namberA = Console.ReadLine();
+Console.WriteLine("Enter second namber");
+string namberB = Console.ReadLine();
+var namber1 = double.Parse(namberA);
+var namber2 = double.Parse(namberB);
+var result = namber1/namber2;
+Console.WriteLine(result);
