659fcc276a73b2004c9058762fc2ff627d587c41
Max Babich <sapienti@list.ru>
2022-12-14 16:12:39 +0300

    example005 added

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Print your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "max") // ToLower() делает все буквы строчными
+    {
+        Console.Write("I'm glad to see you, Max!" );
+    }
+else
+    {
+        Console.WriteLine("Hello ",username);
+    }
